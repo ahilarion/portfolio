4ce509e49915c114b042303d27d86a0eb8c17d71
Alan <alan.hilarion@proton.me>
2024-05-21 00:50:58 +0200

    feat(BioView): add navigation to parent page
    1. Implemented a function goToParent() that navigates back to the personal page within the BioView component.
    2. Linked the function to an SVG icon to trigger the function onClick event.

--- src/views/BioView.vue
+++ src/views/BioView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted  } from 'vue';
+import router from "@/router/index.js";
 
 const textContentRef = ref(null);
 const lineCount = ref(0);
@@ -17,6 +18,10 @@ onMounted(() => {
   countLines();
 });
 
+function goToParent() {
+  router.push("/about-me/personal");
+}
+
 </script>
 
 <template>
@@ -24,9 +29,8 @@ onMounted(() => {
     <div class="h-8 w-full flex items-center justify-start border-b border-stroke">
       <div class="flex items-center justify-start gap-2 border-r border-stroke px-4 h-full">
         <span class="text-text">bio</span>
-        <svg xmlns="http://www.w3.org/2000/svg" class="fill-text w-4 h-4 cursor-pointer" viewBox="0 0 384 512">
-          <path
-              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" class="fill-text w-4 h-4 cursor-pointer" viewBox="0 0 384 512" @click="goToParent">
+          <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
         </svg>
       </div>
     </div>
