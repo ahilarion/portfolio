edd2246427a4697476fb1365f88c8780599e5ad0
Alan <alan.hilarion@proton.me>
2024-05-20 23:27:42 +0200

    feat(components): add props to Folder and File components
    Updated the Folder component to accept `folderName` and `folderColor` properties and included currentColor feature considering these properties. Also added the ability to toggle open/close the folder with the `isOpen` computed property.
    
    Created a new File component that accepts a `to` property for routing purposes. This component was also added to the HobbiesView.
    
    Adjusted the HobbiesView to accommodate these changes.

--- src/views/HobbiesView.vue
+++ src/views/HobbiesView.vue
@@ -1,12 +1,15 @@
 <script setup>
 
 import Folder from "@/components/Folder.vue";
+import File from "@/components/File.vue";
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-start w-full h-8">
-    <Folder to="/about-me/hobbies/sports">
-        <span class="text-text">sports</span>
+    <Folder folder-name="sports" folder-color="fill-amber-300">
+      <File to="/about-me/hobbies/sports">
+        <span class="text-text">sport</span>
+      </File>
     </Folder>
   </div>
 </template>
