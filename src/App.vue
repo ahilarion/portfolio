dd7d206c2fd2b9e7a927cef157ce5a7c29d2eac1
Alan <alan.hilarion@proton.me>
2024-05-20 16:20:27 +0200

    feat(App.vue): Enhance App interface with Footer and blurred gradient animations
    The commit includes the adding of Footer to the App.vue and introduction of blurry gradient animations. Several CSS keyframes and media queries were used to create oscillating animations for the gradients. App.vue organization was also enhanced to accommodate these updates. Existing CSS for the app were modified to make it fit the new elements, both in index.html and App.vue. Footer.vue was created.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,89 @@
 <script setup>
 import Navigation from "@/components/Navigation.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-  <div class="bg-primary w-full border border-stroke rounded">
+  <div class="w-full border border-stroke rounded bg-[rgba(1,22,39,0.8)] flex flex-col items-center">
     <Navigation />
+    <main class="flex-1 w-full flex flex-col items-center justify-center">
+      <router-view />
+    </main>
+    <Footer />
   </div>
+  <div class="blurry-gradient-blue"></div>
+  <div class="blurry-gradient-green"></div>
 </template>
+
+<style scoped>
+@keyframes oscillate {
+  0%, 100% {
+    transform: rotate(10deg);
+  }
+  50% {
+    transform: rotate(-10deg);
+  }
+}
+
+.blurry-gradient-blue, .blurry-gradient-green {
+  animation: oscillate 5s infinite; /* 5s here is the duration of one oscillation cycle */
+}
+
+@media (min-width: 1024px) {
+  .blurry-gradient-blue, .blurry-gradient-green {
+    animation: oscillate 5s infinite;
+  }
+}
+
+.blurry-gradient-blue, .blurry-gradient-green {
+  filter: blur(70px);
+  z-index: 10;
+  position: fixed;
+  width: 300px;
+  height: 300px;
+  opacity: .5;
+}
+
+.blurry-gradient-blue {
+  background: radial-gradient(circle at 50% 50%, #4d5bce, rgba(76, 0, 255, 0));
+  right: 5%;
+  bottom: 25%;
+  border-radius: 0 0 50% 50%;
+  transform: rotate(10deg);
+}
+
+.blurry-gradient-green {
+  background: radial-gradient(circle at 50% 50%, #4dce5b, rgba(0, 255, 76, 0));
+  /* Adjust the position here */
+  right: 15%;
+  bottom: 35%;
+  border-radius: 50% 0 100% 50%;
+  transform: rotate(-10deg);
+}
+
+@media (min-width: 1024px) {
+  .blurry-gradient-blue {
+    border-radius: 100% 50% 100% 0;
+    right: 10%;
+    bottom: 10%;
+    width: 500px;
+    height: 500px;
+    opacity: .7;
+  }
+
+  .blurry-gradient-green {
+    border-radius: 50% 0 50% 100%;
+    right: 30%;
+    bottom: 50%;
+    width: 400px;
+    height: 400px;
+    opacity: .7;
+  }
+}
+
+@media (max-width: 1023px) {
+  .blurry-gradient-blue, .blurry-gradient-green {
+    display: none;
+  }
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer class="w-full h-[50px] flex justify-between border-t border-t-stroke">
+
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
